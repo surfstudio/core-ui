60c494b28ae2a5279786adbb06cf19bfd3286484
Margarita <margo.himera@yandex.ru>
2020-05-26 13:05:25 +0300

    Merge branch 'dev/G-0.5.0' into ANDDEP-853-easyadapter-release (b5575753)

--- core-ui/lib-core-ui/src/main/java/ru/surfstudio/android/core/ui/permission/exceptions/SettingsRationalIsNotProvidedException.kt
+++
@@ -1,6 +0,0 @@
-package ru.surfstudio.android.core.ui.permission.exceptions
-
-/**
- * Исключение, возникающее, если {@link PermissionRequest#settingsRationalStr} равен null.
- */
-class SettingsRationalIsNotProvidedException : RuntimeException("Set settingsRationalStr to non null value.")
